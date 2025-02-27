bf1fd236322fe753d9efbe30593e2c14a023baf5
Jul21ak <cj.avcj1@gmail.com>
2025-02-26 23:09:14 -0500

    Se implementa React y se crean componentes de cada elemento

---
+++ client/src/components/vision/vision.js
@@ -0,0 +1,9 @@
+export function Vision(){
+    return(
+        <div>
+            <a>No veo, estoy ciego!</a>
+        </div>
+    )
+}
+
+export default Vision
--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import counterReducer from './redux/reducer';
+import { applyMiddleware, compose, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import rootReducer from "../Reducer";
 
-// Crear el store de Redux
-const store = configureStore(counterReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+export default store;
